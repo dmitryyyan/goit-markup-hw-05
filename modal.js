ce43b6a8fdb313e678c7aad90abcd36e62f3c525
dmitryyyan <dima2003yan@gmail.com>
2023-10-19 21:08:28 +0300

    new

---
+++ modal.js
@@ -0,0 +1,7 @@
+document.getElementById("modal-form-btn").addEventListener("click", function () {
+	document.getElementById("modal").classList.add("is-open")
+})
+
+document.getElementById("modal-btn").addEventListener("click", function () {
+	document.getElementById("modal").classList.remove("is-open")
+})
